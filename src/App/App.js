b22dc10b59f917795f9204759fcf2dcdb9cf99fe
timosha <tinaatimkovaa@gmail.com>
2020-11-25 14:43:43 -0500

    complete restaurant cards component

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,35 @@
-import { Fragment } from 'react'
 import './App.css'
 import RestaurantCard from '../RestaurantCard/RestaurantCard.js'
 import { data } from '../mockData.js'
 
-const App = () => {
-  return(
-    <Fragment>
-    <div className='restaurant-img'>
-      <h2 className='header'>Find your restaurant for any occasion</h2>
-      <form className='search'>
-        <label htmlFor='search'></label>
-        <input className='search-bar' autoComplete='off' type='text' id='search' name='search' placeholder='Location, Restaurant, or Cuisine'/>
-        <button className='submit-btn' type='submit'>Let's Go</button>
-      </form>
-    </div>
-    <div>
-    {data.map((restaurant, index) => (
-      <li key={index}>
-        <RestaurantCard restaurant={restaurant} />
-      </li>
-    ))}
-    </div>
-    </Fragment>
-  )
+class App extends Component {
+
+  state={
+    restaurants: [],
+    loading: false
+  }
+
+  render() {
+    return(
+      <main className='mainContent'>
+      <header className='restaurant-img'>
+        <h2 className='header'>Find your restaurant for any occasion</h2>
+        <form className='search'>
+          <label htmlFor='search'></label>
+          <input className='search-bar' autoComplete='off' type='text' id='search' name='search' placeholder='Location, Restaurant, or Cuisine'/>
+          <button className='submit-btn' type='submit'>Let's Go</button>
+        </form>
+      </header>
+      <section className='cards'>
+      {data.map((restaurant, index) => (
+        <li key={index}>
+          <RestaurantCard restaurant={restaurant} />
+        </li>
+      ))}
+      </section>
+      </main>
+    )
+  }
 }
 
 export default App;
--- src/RestaurantCard/RestaurantCard.js
+++ src/RestaurantCard/RestaurantCard.js
@@ -4,6 +4,12 @@ const RestaurantCard = ({ restaurant }) => {
       <div>
         <img src={restaurant.image} />
         <h4>{restaurant.name}</h4>
+        <p>{restaurant.rating}</p>
+        <ul>
+          <li>{restaurant.cuisine}</li>
+          <li>{restaurant.price}</li>
+          <li>{restaurant.location}</li>
+        </ul>
       </div>
     )
 }
