42459520b86e7f7a8b9293b050bb92e0db007a15
timosha <tinaatimkovaa@gmail.com>
2020-11-27 22:58:12 -0500

    latest changes

--- src/RestaurantCard/RestaurantCard.js
+++ src/RestaurantCard/RestaurantCard.js
@@ -1,19 +1,25 @@
 import react, { Fragment } from 'react'
 
 const RestaurantCard = ({ restaurants }) => {
+  if (restaurants.length) {
     return(
       <Fragment>
-        {console.log(restaurants)}
-        {/* <img src={restaurant.image} /> 
-        <h4>{restaurant.name}</h4>
-        <p>{restaurant.rating}</p>
+        <h4>{restaurants.name}</h4>
+        {/*<p>{restaurant.rating}</p>
         <ul>
           <li>{restaurant.cuisine}</li>
           <li>{restaurant.price}</li>
           <li>{restaurant.location}</li>
-        </ul>*/}
+    </ul>*/}
       </Fragment>
+  )
+  } else {
+    return (
+    <h4>'Loading...'
+      {console.log(restaurants)}
+    </h4>
     )
+  }    
 }
 
 export default RestaurantCard
