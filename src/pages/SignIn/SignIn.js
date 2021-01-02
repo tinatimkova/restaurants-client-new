328e389656d180c67064623135bfdb41301cbf3d
timosha <tinaatimkovaa@gmail.com>
2021-01-01 22:27:33 -0500

    add material ui for sign in form

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,26 +1,19 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button';
+import './SignIn.module.css'
 
 function SignIn({ onClose }) {
     return (
         <div className='modal' id='modal'>
-            <form className='content'>
-                <label>
-                <input type='text' name='first_name' />
-                </label>
-                <label>
-                <input type='text' name='last_name' />
-                </label>
-                <label>
-                <input type='text' name='email' />
-                </label>
-                <label>
-                <input type='text' name='password' />
-                </label>
-                <label>
-                <input type='text' name='password_confirmation' />
-                </label>
+            <form className='sign-in-form'>
+                <TextField type='text' name='first_name' label='First name' variant="outlined" />
+                <TextField type='text' name='last_name' label='Last name' variant="outlined" />
+                <TextField type='email' name='email' label='Email' variant="outlined" />
+                <TextField type='password' name='password' variant="outlined" label='Password'/>
+                <TextField type='password' name='password_confirmation' label='Password Confirmation' variant="outlined" />
                 <div className='actions'>
-                <button className='toggle-button' type='submit' onClose={() => onClose(false)}>Create an account</button>
+                <Button variant='primary' className='toggle-button' type='submit' onClose={() => onClose(false)}>Create an account</Button>
                 </div>
             </form>
         </div>
