1f400b266012a0a761cf8eaa3b4b3c7ab4c442a6
timosha <tinaatimkovaa@gmail.com>
2021-01-03 12:13:16 -0500

    start sign up form functionality

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import styles from './SignUp.module.css'
 
 function SignUp() {
+    const [formData, setFormData] = useState({first_name: '', last_name: '', email: '', password: ''})
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formData)
+    }
+
+    const handleChange = (e) => {
+        setFormData({ [e.target.name]: e.target.value })
+    }
+
     return (
         <>
-            <form className={styles['sign-up-form']}>
+            <form className={styles['sign-up-form']} onSubmit={handleSubmit} >
                 <h3>Please sign up</h3>
                 <hr/>
-                <TextField type='text' name='first_name' label='First name' variant="outlined" />
-                <TextField type='text' name='last_name' label='Last name' variant="outlined" />
-                <TextField type='email' name='email' label='Email' variant="outlined" />
-                <TextField type='password' name='password' variant="outlined" label='Password'/>
-                <TextField type='password' name='password_confirmation' variant="outlined" label='Re-enter password'/>
+                <TextField type='text' name='first_name' label='First name' variant="outlined" value={formData.first_name} onChange={handleChange} required />
+                <TextField type='text' name='last_name' label='Last name' variant="outlined" value={formData.last_name} onChange={handleChange} />
+                <TextField type='email' name='email' label='Email' variant="outlined" value={formData.email} onChange={handleChange} required />
+                <TextField type='password' name='password' variant="outlined" label='Password' value={formData.password} onChange={handleChange} required />
+                <TextField type='password' name='password_confirmation' variant="outlined" label='Re-enter password' required />
                 <Button variant='contained' className='toggle-button' type='submit'>Create an account</Button>
             </form>
         </>
