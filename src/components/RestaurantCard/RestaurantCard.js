0b9b172d0a57bf4f5ff48355fc0710d8dd604e4b
timosha <tinaatimkovaa@gmail.com>
2021-01-07 14:26:36 -0500

    wrap img with an <a> tag

--- src/components/RestaurantCard/RestaurantCard.js
+++ src/components/RestaurantCard/RestaurantCard.js
@@ -9,15 +9,17 @@ const RestaurantCard = ({ restaurants, loading }) => {
   } else {
     return(
       restaurants.map(i => (
-        <a href={i.restaurant.url}  key={i.restaurant.id} target='_blank'><ul className={styles['restaurant-card']}>
+        <ul className={styles['restaurant-card']} key={i.restaurant.id}>
+          <a href={i.restaurant.url} target='_blank' rel="noreferrer">
           <img className={styles['card-img']} src={i.restaurant.featured_image.length !== 0 ? i.restaurant.featured_image : placeholder } alt='featured restaurant'/>
+          </a>
           <div className={styles['restaurant-info']}>
           <li className={styles['name']}>{i.restaurant.name}</li>
           <li>{i.restaurant.location.city}</li>
           <li className={styles["stars"]} style={{'--rating': `${i.restaurant.user_rating.aggregate_rating}`}} aria-label={`Rating of this restaurant is ${i.restaurant.user_rating.aggregate_rating} out of 5.`}><span className={styles['reviews']}>{i.restaurant.all_reviews_count} reviews</span></li>
           <li>{i.restaurant.cuisines}<span className={styles['dollars']} style={{'--price': `${i.restaurant.price_range}`}} aria-label={`Price range of this restaurant is ${i.restaurant.price_range} out of 4.`}></span></li>
           </div>
-        </ul></a>
+        </ul>
       ))
     )
   }
